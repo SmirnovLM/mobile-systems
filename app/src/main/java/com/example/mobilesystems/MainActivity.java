5fc0387387469d1766e27423eeca4cc1386b0f21
SmirnovLM <smirnoff.2003@yandex.ru>
2024-04-19 14:34:36 +0300

    doing the lab2

--- app/src/main/java/com/example/mobilesystems/MainActivity.java
+++ app/src/main/java/com/example/mobilesystems/MainActivity.java
@@ -1,19 +1,57 @@
 package com.example.mobilesystems;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView counterTextView;
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        counterTextView = findViewById(R.id.counterTextView);
+        Button incrementButton = findViewById(R.id.incrementButton);
+        Button decrementButton = findViewById(R.id.decrementButton);
+        Button resetButton = findViewById(R.id.resetButton);
+
+        // Обработчик клика на кнопку "Прибавить единицу"
+        incrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                updateCounter();
+            }
+        });
+
+        // Обработчик клика на кнопку "Отнять единицу"
+        decrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter--;
+                updateCounter();
+            }
+        });
+
+        // Обработчик клика на кнопку "Обнулить"
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter = 0;
+                updateCounter();
+            }
+        });
     }
+
+    // Метод для обновления текста счетчика
+    private void updateCounter() {
+        counterTextView.setText(String.valueOf(counter));
+    }
+
 }
