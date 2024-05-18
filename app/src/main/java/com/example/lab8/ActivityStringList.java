509bd044f11f10f1b63635fd21c7e7bf5ee5ace8
SmirnovLM <smirnoff.2003@yandex.ru>
2024-05-18 12:25:55 +0300

    add alert with word information for StringLust

--- app/src/main/java/com/example/lab8/ActivityStringList.java
+++ app/src/main/java/com/example/lab8/ActivityStringList.java
@@ -1,12 +1,19 @@
 package com.example.lab8;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Objects;
@@ -29,6 +36,8 @@ public class ActivityStringList extends AppCompatActivity {
         Button buttonRemove = findViewById(R.id.buttonRemove);
         textViewAllWords = findViewById(R.id.textViewAllWords);
 
+        textViewAllWords.setMovementMethod(LinkMovementMethod.getInstance());
+
         buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,7 +49,6 @@ public class ActivityStringList extends AppCompatActivity {
                 }
             }
         });
-
         buttonRemove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,7 +59,67 @@ public class ActivityStringList extends AppCompatActivity {
     }
 
     private void updateAllWordsTextView() {
-        textViewAllWords.setText(stringList.getAllWordsAsString());
+        SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder();
+        for (int i = 0; i < stringList.getStringList().size(); i++) {
+            String originalWord = stringList.getStringList().get(i);
+            String word = originalWord;
+            if (i == 0) {
+                word = word.substring(0, 1).toUpperCase() + word.substring(1);
+            }
+
+            SpannableString spannableString = new SpannableString(word);
+            final int index = i;
+            ClickableSpan clickableSpan = new ClickableSpan() {
+                @Override
+                public void onClick(@NonNull View widget) {
+                    showWordInfo(originalWord, index);
+                }
+            };
+
+            spannableString.setSpan(clickableSpan, 0, word.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            spannableStringBuilder.append(spannableString);
+            if (i < stringList.getStringList().size() - 1) {
+                spannableStringBuilder.append(", ");
+            }
+        }
+        textViewAllWords.setText(spannableStringBuilder);
+    }
+
+    private void showWordInfo(String word, int index) {
+
+        String message =
+                "Порядковое место: " + (index + 1) + "\n" +
+                "Длина: " + word.length() + "\n" +
+                "Количество гласных: " + countVowels(word) + "\n" +
+                "Количество согласных: " + countConsonants(word);
+
+        new AlertDialog.Builder(this)
+                .setTitle("Информация о слове")
+                .setMessage(message)
+                .setPositiveButton("OK", (dialog, which) -> updateAllWordsTextView())
+                .show();
+    }
+
+    private int countVowels(String word) {
+        int count = 0;
+        String vowels = "aeiouyаеёиоуыэюя";
+        for (char c : word.toLowerCase().toCharArray()) {
+            if (vowels.indexOf(c) != -1) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private int countConsonants(String word) {
+        int count = 0;
+        String consonants = "bcdfghjklmnpqrstvwxyzбвгджзйклмнпрстфхцчшщ";
+        for (char c : word.toLowerCase().toCharArray()) {
+            if (consonants.indexOf(c) != -1) {
+                count++;
+            }
+        }
+        return count;
     }
 
     @Override
